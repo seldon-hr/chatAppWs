81537f6277473ea3c9f6affd5691f512fe28d3a6
seldon-hr1 <daanmaro99june@gmail.com>
2024-07-05 14:57:21 -0600

    feat(db)"Test the connection by server and the db"

---
+++ src/server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const connectDB = require('../config/database');
+
+dotenv.config();
+
+const app = express();
+
+
+//Middleware
+app.use(cors());
+app.use(express.json());
+
+//Connect to database
+connectDB();
+
+//Routes
+app.get('/', (req, res) => {
+    res.send('Chat App Ws is running');
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
