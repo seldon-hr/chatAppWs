e8371e138b613ee2e04a06bba4c9cda55ab2c2a4
seldon-hr1 <daanmaro99june@gmail.com>
2024-10-28 16:25:07 -0600

    Configured the structure of the server and the database

--- config/database.js
+++ config/database.js
@@ -2,10 +2,30 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI);
+        const connection = await mongoose.connect(process.env.MONGO_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            serverSelectionTimeoutMS: 5000,
+            socketTimeoutMS: 45000,
+        })
         console.log('MongoDB connected');
+
+        //Manejo de eventos de conexión
+        mongoose.connection.on('connected', () => {
+            console.log('Mongoose connecte to DB');
+        });
+
+        mongoose.connection.on('error', (err) => {
+            console.log('Mongoose connection error', err);
+        });
+
+        mongoose.connection.on('disconnected', () => {
+            console.log('Mongoose connection disconnected');
+        });
+
+
     } catch (error) {
-        console.error('MongoDB connection error: ', error);
+        console.error('MongoDB connection main error: ', error);
         process.exit(1);
     }
 };
--- server.js
+++ server.js
@@ -2,11 +2,13 @@ const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
 const connectDB = require('./config/database');
+const { status } = require('express/lib/response');
 
 /* Rutas y errorHandler */
 /* const routes = require('../routes/')
 const errorHandler = require('../middlewares/errorHandler') */
 
+//Configuración de variables de entorno
 dotenv.config();
 
 //Iniciar app de express
@@ -23,38 +25,34 @@ app.use(express.urlencoded({ extended: true }));
 //Connect to database
 connectDB();
 
-/* //Routes
+//Routes
 app.get('/', (req, res) => {
-    res.send('Chat App Ws is running');
-}); */
+    res.json({ message:'Chat App Ws is running',  status: "success" });
+});
+
+//Manejo de errores globales
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).json({
+        success: false,
+        message: err.message || 'Server Error',
+        status: process.env.NODE_ENV === 'development' ? err : {},
+    });
+});
 
 
 
 //Inicio del servidor
 const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+const server =  app.listen(PORT, () => {
+    console.log(`Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`);
 });
 
-
-
-const mongoose = require('mongoose');
-const connectDB = async () => {
-    try {
-        const connection = await mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-            useCreateIndex: true,
-            serverSelectionTimeoutMS: 5000
-        });
-        console.log('MongoDB connected: ', connection.connection.host);
-    } catch (error) {
-        console.error('MongoDB connection error: ', error);
-        process.exit(1);
-    }
-};
-
-module.exports = connectDB;
+//Manejo de errores no capturados
+process.on('unhandledRejection', (err, promise) => {
+    console.log(`Error: ${err.message}`);
+    //Cerrar el servidor y salir del proceso
+    server.close(() => process.exit(1));
+});
 
 
