9469ad15fc845c1f7336576ff302df3bb3ce11b7
Seldon <daanmaro99june@gmail.com>
2024-12-18 12:15:49 -0600

    fix(database): enhance error handling for MongoDB connection issues and log public IP for whitelisting

--- config/database.js
+++ config/database.js
@@ -4,7 +4,8 @@ const connectDB = async () => {
     try {
         /* Al momento de realizar la conexión corroborar que se puede obtener acceso desde un ip disponible,
             de lo contrario checar en el módulo de mongo Atlas -> Network access */
-        console.log('process.env.MONGO_URI:', process.env.MONGO_URI);
+        
+        // console.log('process.env.MONGO_URI:', process.env.MONGO_URI);
         const connex = await mongoose.connect(process.env.MONGO_URI, {})
         console.log('MongoDB connected: ', connex.connection.host);
 
@@ -30,7 +31,33 @@ const connectDB = async () => {
 
 
     } catch (error) {
+        /* Gestión de errores de conexión a la db */
         console.error('MongoDB connection main error: ', error);
+        /* Devolver el error si este es a raíz de que la ip no se encuentra en la lista blanca
+            de ip's autorizadas */
+        const NO_ENCONTRADA_URL_CLUSTER = 'ENOTFOUND';
+        const ERROR_CREDENTIALS = error.name === 'MonooseError' && error.message.includes('authentication failed');
+        const NO_IP_AUTORIZADA_LISTA_BLANCA = (error.name === 'MongooseServerSelectionError');
+
+        if (error.code === NO_ENCONTRADA_URL_CLUSTER) {
+            console.error('Error de DNS: No se encontró la URL del cluster.');
+        } else if (ERROR_CREDENTIALS) {
+            console.error('Error de Usuario o Contraseña, verficar credenciales.');
+        } else if (NO_IP_AUTORIZADA_LISTA_BLANCA) {
+            console.error('❗⚠️ Posible IP no autorizada, Verifica la lista bla nca de MongoDB Atlas Netwokr Access');
+            console.info('Tu IP actual necesita ser agregada a la whitelist')
+
+             // Podemos intentar obtener la IP pública
+            try {
+                const response = await fetch('https://api.ipify.org?format=json');
+                console.log(response)
+                const data = await response.json();
+                console.error(`IP a agregar: ${data.ip}`);
+            } catch (ipError) {
+                console.error('No se pudo obtener la IP pública');
+            }
+        }
+
         process.exit(1);
     }
 };
