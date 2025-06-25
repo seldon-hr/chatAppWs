4c731945470b8db9e0e130ddfb9923e497b31ff6
Seldon <daanmaro99june@gmail.com>
2025-06-25 17:09:40 -0600

    fix(channels): Prototipo de Canales.

--- controllers/channelsController.js
+++ controllers/channelsController.js
@@ -7,6 +7,7 @@ exports.getChannels = async (request, response) => {
         await Canal.find({})
             .then(canales => {
                 channels = canales.map(canal => canal.toPublicJSON());
+                console.log(`Canales obtenidos:', ${channels.length})`);
             })
             .catch(error => {
                 console.log('Error desde MongoDB al recuperar los canales.', error);
@@ -38,3 +39,38 @@ exports.getChannels = async (request, response) => {
 
 
 /* Get Channels by user */
+exports.getChannelsByUser = async (request, response) => {
+    console.log(`Bùsqueda de canales para el usuarios ${request.userId}`);
+    try {
+        let channels = [];
+        /* TODO: Crear estructura de Canal */
+        await Canal.find({}) //Incorporar el body del usuario.
+            .then(canales => {
+                channels = canales.map(canal => canal.toPublicJSON());
+                console.log(`Canales obtenidos:', ${channels.length})`);
+            })
+            .catch(error => {
+                console.log('Error desde MongoDB al recuperar los canales.', error);
+            })
+        
+        if (channels.length == 0) {
+        return response.status(401).json({
+            success: false,
+            messages: 'No se encontraron canales.'
+        })
+        }
+
+        response.status(200).json({
+            success: true,
+            body: channels
+        }) 
+           
+
+    } catch (error) {
+        console.log('Error al obtener los canales', error);
+        response.status(500).json({
+            success: false,
+            message: 'Error en el servidor',
+        });
+    }
+}
