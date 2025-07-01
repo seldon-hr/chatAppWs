38034a207ef8c22f76d05752fde36cfe28c6d9b5
Seldon <daanmaro99june@gmail.com>
2025-07-01 16:34:00 -0600

    feat(channels): add ChannelUser model for user-channel relationships

---
+++ models/ChannelUser.js
@@ -0,0 +1,40 @@
+//Modelando Canal
+const mongoose = requiere('mongoose');
+
+const channelUserSchema = new mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        unique: true,
+    },
+    channelId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Channel",
+        unique: true,
+    },
+    joinedAt: {
+        type: Date,
+        default: Date.now,
+    },
+    lastMessageReadId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Message",
+        unique: true,
+    },
+    notificationsMute: Boolean,
+}, {
+    timestamps: true,
+});
+
+channelUserSchema.methods.toPublicJSON = function () {
+    return {
+        userId: this.userId,
+        channelId: this.channelId,
+        joinedAt: this.joinedAt,
+        lastMessageReadId: this.lastMessageReadId,
+        notificationsMute: this.notificationsMute,
+    }
+}
+
+
+module.exports = mongoose.model('ChannelUser', channelUserSchema);
