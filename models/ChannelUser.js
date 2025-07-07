c476fc0f3ee9fcd6f1f2ce44837bf04daabf0085
Seldon <daanmaro99june@gmail.com>
2025-07-06 20:32:43 -0600

    fix(channels): update comments to clarify the use of composite indexes for user-channel relationships

--- models/ChannelUser.js
+++ models/ChannelUser.js
@@ -53,3 +53,14 @@ channelUserSchema.methods.toPublicJSON = function () {
 
 
 module.exports = mongoose.model('ChannelUser', channelUserSchema);
+
+
+/* 
+ Dentro del uso de los canales hicimos uso de índices compuestos que a diferencia del inicio los manejábamos como si fueron todos únicos, en relación a `channelId` y `userId`, donde estos impedian que se volviera a crear una relación tipo de un usuario tuviera ese canal, entonces teníamos que cambiar que ese no era el tipo de unicidad que necestibamos. Donde, el único registro que no se puede replicar es cuando ocurre este caso `{ userId - channelId }`, esto se traduce como seldon - general. Lo  cual nos dice que ya no buscamos que exista la relación del usuario seldon con el canal general más de una vez. 
+
+Por ende procedimos a eliminar los índices únicos de estas props únicas. Y crear el índice compuesto. Este es una vez realizado el model dentro del schema.
+
+```html
+channelUserSchema.index({ channelId, userId }, unique: true );
+```
+*/
