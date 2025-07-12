d9bd2e1593a8833d40479a8596ac4198f9cd3f0c
Seldon <daanmaro99june@gmail.com>
2025-07-11 20:07:37 -0600

    feat(gemini): Consumir gemma por medio de chat de contexto.

--- controllers/chatAIController.js
+++ controllers/chatAIController.js
@@ -7,18 +7,20 @@ este archivo por node 'nombredodearchivo.js', así cargamos el env
 a process.env, de lo contrario cuando es el uso de la app, se cargan desde
 server.js
  */
-/* const dotenv = require('dotenv');
-dotenv.config({ path: '../.env'}); */
+const dotenv = require('dotenv');
+dotenv.config({ path: '../.env'});
 
-const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 console.log('API KEY', process.env.GEMINI_API_KEY);
+console.log('AI MODEL', process.env.AI_MODEL);
+const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 
 
-let prompt = 'Hola, Gemini, te estoy consumiendo desde un env de node.js con express. Pero solamente por el momento, ejecutando la función base que se conecta a la API.'
+let prompt = 'Hola, Gemma, te estoy consumiendo desde tu API, por un archivo node.js, son mis primeros pasos usando la API.'
+let newPrompt = '¿Cuáles serían los siguientes pasos?';
 
 async function getGeminiChat(prompt) {
     console.log('Starting request... 🚀')
-    const model = genAI.getGenerativeModel({ model: /* "gemini-1.5-flash" */ "gemma-3n-e2b-it" });
+    const model = genAI.getGenerativeModel({ model: process.env.AI_MODEL });
 
     try {
         const result = await model.generateContent(prompt);
@@ -34,6 +36,55 @@ async function getGeminiChat(prompt) {
         console.error('Error al conectar con Gemini ✨');
     }
 }
+
+/* Chat continuo */
+async function getChatRun(newPrompt/* , previousChats */) {
+    console.log('Starting Chat Run request... 🚀')
+    const model = genAI.getGenerativeModel({ model: process.env.AI_MODEL });
+
+    try {
+
+        //Obtenemos el chat primero en una búsqueda por canal posteriormente.
+        //* Este chat se debe guardar en la base, con esta estructura y lo traeremos
+        // del propio previousChat.
+        const chat = model.startChat({
+            history: /* previousChats */[
+                {
+                    role: 'user',
+                    parts: [{
+                        text: prompt
+                    }]
+                },
+                {
+                    role: 'model',
+                    parts: [{
+                        text: 'Hola, Dan, me alegró, un gusto conocerte. Que Alegría que estes dando tus primeros pasos.',
+                    }]
+                },
+            ],
+            generationConfig: {
+                maxOutputTokens: 100,
+            },
+        });
+
+
+        const result = await model.generateContent(newPrompt);
+        const response = await result.response;
+        if (response) {
+            const text = response.text();
+            console.log(text);
+            return text;
+        }
+    } catch (error) {
+        console.error('Error al conectar con Gemini ✨');
+    }
+}
+
+/* getChatRun(newPrompt); */
+
+
+
+
 /* TEsting */
 /* getGeminiChat(prompt); */
 
